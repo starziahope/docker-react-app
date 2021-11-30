9df6b6d2060874575200ef96a5dcd5074c5fdac7
starziahope <ddddasd1@naver.com>
2021-11-30 22:39:59 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          테스트코드입니다
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
